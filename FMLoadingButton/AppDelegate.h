e8b461847122ef6125f99e87533afd6488a4420f
fanmaoyu0871 <frank0871@163.com>
2016-02-22 11:10:06 +0800

    Initial Commit

---
+++ FMLoadingButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FMLoadingButton
+//
+//  Created by 范茂羽 on 16/2/22.
+//  Copyright © 2016年 范茂羽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FMLoadingButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FMLoadingButton
+//
+//  Created by 范茂羽 on 16/2/22.
+//  Copyright © 2016年 范茂羽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
